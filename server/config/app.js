c8765fd0a04234d1d5573fcc57dd9ea0d2fbad0f
Hashir416 <msheikhashir@gmail.com>
2024-11-23 21:19:41 -0500

    Commit again

--- server/config/app.js
+++ server/config/app.js
@@ -7,59 +7,50 @@ let logger = require('morgan');
 let app = express();
 let indexRouter = require('../routes/index');
 let usersRouter = require('../routes/users');
-let bookRouter = require('../routes/book');
-
+let WtrackerRouter = require('../routes/Wtracker');
 
 // view engine setup
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', path.join(__dirname, '../views')); // No changes needed here
 app.set('view engine', 'ejs');
-// getting-started.js
+
+// MongoDB setup
 const mongoose = require('mongoose');
 let DB = require('./db');
-// point mongoose to the DB URI
+// Point mongoose to the DB URI
 mongoose.connect(DB.URI);
 let mongoDB = mongoose.connection;
-mongoDB.on('error',console.error.bind(console,'Connection Error'));
-mongoDB.once('open',()=>{
-  console.log("Connected with the MongoDB")
+mongoDB.on('error', console.error.bind(console, 'Connection Error'));
+mongoDB.once('open', () => {
+  console.log('Connected to MongoDB');
 });
-mongoose.connect(DB.URI,{useNewURIParser:true,useUnifiedTopology:true})
-/* main().catch(err => console.log(err));
-
-async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/BookLib');
-  //await mongoose.connect('mongodb+srv://ahmedsheikh:Test123@cluster0.0f3pz.mongodb.net/');
-
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-}*/
+mongoose.connect(DB.URI, { useNewURIParser: true, useUnifiedTopology: true });
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../../public')));
+app.use(express.static(path.join(__dirname, '../../public'))); // Static files
 app.use(express.static(path.join(__dirname, '../../node_modules')));
 
+// Define routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/bookslist',bookRouter);
-// /project --> projectrouter
-// /contactus --> contactus
+app.use('/workouts-list', WtrackerRouter); // Updated from books-list to workouts-list
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+// Error handler
+app.use(function (err, req, res, next) {
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
-  res.render('error',{title:'Error'});
+  res.render('error', { title: 'Error' }); // Updated to ensure consistent title formatting
 });
 
 module.exports = app;
