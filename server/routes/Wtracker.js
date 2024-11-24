fc5e379f7f2c9ac694a0d977d9bebb0bc8685a6f
Hashir416 <msheikhashir@gmail.com>
2024-11-23 20:46:45 -0500

    Read operation change made

--- server/routes/Wtracker.js
+++ server/routes/Wtracker.js
@@ -2,30 +2,30 @@ var express = require('express');
 var router = express.Router();
 let mongoose = require('mongoose');
 // telling my router that I have this model
-let Book = require('../model/book');
-const book = require('../model/book');
-let bookController = require('../controllers/book.js')
-/* Get route for the book list - Read Operation */
+let Workout = require('../model/Wtracker.js');
+let workoutController = require('../controllers/Wtracker.js');
+
+/* Get route for the workout tracker - Read Operation */
 /*
 GET,
 Post,
 Put --> Edit/Update
 */
-/* Read Operation --> Get route for displaying the books list */
-router.get('/',async(req,res,next)=>{
-try{
-    const BookList = await Book.find();
-    res.render('Book/list',{
-        title:'Books',
-        BookList:BookList
-    })}
-    catch(err){
+/* Read Operation --> Get route for displaying the Workouts */
+router.get('/', async (req, res, next) => {
+    try {
+        const WorkoutList = await Workout.find(); 
+        res.render('Workout/list', { 
+            title: 'Workouts', 
+            WorkoutList: WorkoutList 
+        });
+    } catch (err) {
         console.error(err);
-        res.render('Book/list',{
-            error:'Error on the server'
-        })
+        res.render('Workout/list', { 
+            error: 'Error on the server'
+        });
     }
-    });
+});
 /* Create Operation --> Get route for displaying me the Add Page */
 router.get('/add',async(req,res,next)=>{
     try{
